53e9a0da7a963da0d7b0763ebf3e198dc8d08d32
Necritto <onetopyr@mail.ru>
2020-01-21 23:11:38 +0300

    Feature: implemented positioning of validation errors [ref:no-ref]

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -24,14 +24,20 @@ more.addEventListener('click', (e) => {
   e.preventDefault();
   more.querySelector('.dot').classList.toggle('triangle');
   moreItem.classList.toggle('show');
+
+
+  // Additional navbar positioning
+
+
+
 });
 
 //  Login window
 
 const login = $nav.querySelector('.login');
 
-//login.addEventListener('click', modal);
-modal();
+login.addEventListener('click', modal);
+
 function modal() {
   const div = document.createElement('div');
   div.classList.add('modal');
@@ -58,34 +64,52 @@ function modal() {
 
   // Form validate
 
-  {
-    const logErr = $nav.querySelector('.logErr');
-    const passErr = $nav.querySelector('.passErr');
-    const form = $nav.querySelector('.form');
-    const loginInput = $nav.querySelector('input[type="text"]');
-    const passInput = $nav.querySelector('input[type="password"]');
+  const logErr = $nav.querySelector('.logErr');
+  const passErr = $nav.querySelector('.passErr');
+  const form = $nav.querySelector('.form');
+  const loginInput = $nav.querySelector('input[type="text"]');
+  const passInput = $nav.querySelector('input[type="password"]');
 
-    form.addEventListener('submit', (e) => {
-      let logMessages = [];
-      let passMessages = [];
+  form.addEventListener('submit', (e) => {
+    let logMessages = [];
+    let passMessages = [];
 
-      (loginInput.value.length < 3) ? logMessages.push('Login must be at least 2 characters')
-        : (loginInput.value.length > 10) ? logMessages.push('Login must be less than 10 characters') : '';
+    (loginInput.value.length < 3) ? logMessages.push('Login must be at least 2 characters')
+      : (loginInput.value.length > 10) ? logMessages.push('Login must be less than 10 characters') : '';
 
-      (passInput.value.length < 8) ? passMessages.push('Password must be at least 8 characters')
-        : (passInput.value.length > 20) ? passMessages.push('Password must be less than 20 characters') : '';
+    (passInput.value.length < 8) ? passMessages.push('Password must be at least 8 characters')
+      : (passInput.value.length > 20) ? passMessages.push('Password must be less than 20 characters') : '';
 
-      if (logMessages.length > 0 || passMessages.length > 0) {
-        e.preventDefault();
-        logErr.innerHTML = logMessages;
-        passErr.innerHTML = passMessages;
-      }
+    if (logMessages.length > 0 || passMessages.length > 0) {
+      e.preventDefault();
+      logErr.innerHTML = logMessages;
+      passErr.innerHTML = passMessages;
+    }
 
-      (!logMessages.length) ? logErr.style.display = 'none' : logErr.style.display = 'block';
-      (!passMessages.length) ? passErr.style.display = 'none' : passErr.style.display = 'block';
-    });
+    (!logMessages.length) ? logErr.style.display = 'none' : logErr.style.display = 'block';
+    (!passMessages.length) ? passErr.style.display = 'none' : passErr.style.display = 'block';
+  });
+
+  // Error validation message positioning
+
+  function createMessageUnder(anchor, elem) {
+    let coords = getCoords(anchor);
+
+    elem.style.left = coords.left + coords.offsetWidth + 'px';
+    elem.style.top = (coords.top + coords.offsetHeight) + 'px';
+    anchor.style.marginBottom = 0;
   }
+
+  createMessageUnder(loginInput, logErr);
+  createMessageUnder(passInput, passErr);
 }
 
+function getCoords(elem) {
 
+  let box = elem.getBoundingClientRect();
 
+  return {
+    top: box.top + pageYOffset,
+    left: box.left + pageXOffset
+  };
+}
